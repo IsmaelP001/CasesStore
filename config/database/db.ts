16fb5758a495fefc57b19df322ce1ad38ab121f2
Nuevo Nombre <p.ismaelp.g1996@gmail.com>
2025-03-20 15:41:37 -0400

    fixed vercion commit

--- config/database/db.ts
+++ config/database/db.ts
@@ -3,10 +3,11 @@ import * as schema from "./schemes";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { config } from "dotenv";
-
 config({ path: ".env.local" });
 
-const queryClient = postgres(process.env.DB_URL!);
-const db =drizzle(queryClient, { schema })
-  
+const db_connection = 
+  process.env.NODE_ENV === "development" ? process.env.DB_URL: process.env.POSTGRES_URL
+const queryClient = postgres(db_connection!);
+const db = drizzle(queryClient, { schema })
+
 export { db };
