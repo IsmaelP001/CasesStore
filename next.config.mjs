fc33ea255fd4a937eb89cb29b0fac59736b4e23d
Nuevo Nombre <p.ismaelp.g1996@gmail.com>
2025-03-23 21:41:07 -0400

    rollback actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig = {
   images: {
@@ -25,21 +24,8 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  webpack: (config, { isServer, webpack }) => {
-    // Ejemplo: Agregar un loader para archivos .txt
-    config.module.rules.push({
-      test: /\.txt$/,
-      use: 'raw-loader', // o el loader que necesites
-    });
 
-    // Puedes seguir modificando el config según tus necesidades
-
-    return config;
-  },
 };
 
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 export default nextConfig;
